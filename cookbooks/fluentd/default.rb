0a12ea9340a3e6cf362235b0dab98a9170f26fe4
yami_beta <yami-beta@users.noreply.github.com>
2016-07-19 14:41:51 +0900

    Fix resource name

--- cookbooks/fluentd/default.rb
+++ cookbooks/fluentd/default.rb
@@ -10,7 +10,7 @@ execute "td-agent_chown" do
   command "chown -R td-agent:td-agent /etc/td-agent"
 end
 
-file "/etc/td-agent/td-agent.conf" do
+remote_file "/etc/td-agent/td-agent.conf" do
   owner "td-agent"
   group "td-agent"
 end
